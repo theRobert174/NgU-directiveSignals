5c0c4c0b18df322d94a67b2b7b8f7d09db0e397b
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-07-09 00:03:46 -0600

    signal mutation

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './properties-page.component.html',
   styleUrls: ['./properties-page.component.css']
 })
 export class PropertiesPageComponent {
+
+  public user = signal<User>({
+    id: 2,
+    email: 'janet.weaver@reqres.in',
+    first_name: 'Janet',
+    last_name: 'Weaver',
+    avatar: 'https://reqres.in/img/faces/2-image.jpg'
+  });
+
+  onFieldUpdated(field: keyof User, value: string){
+    this.user.mutate( current => {
+      switch(field){
+        case 'email':
+          current.email = value;
+          break;
+        case 'first_name':
+          current.first_name = value;
+          break;
+        case 'last_name':
+          current.last_name = value;
+          break;
+        case 'id':
+          current.id = Number(value);
+          break;
+        case 'avatar':
+          current.avatar = value;
+          break;
+      }
+    });
+  }
 
 }
