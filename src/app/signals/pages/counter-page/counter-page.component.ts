d31b86dfb34f7eed22a282351c210165b9339a5a
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-07-08 22:26:30 -0600

    counter signal n computed

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   templateUrl: './counter-page.component.html',
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
 })
 export class CounterPageComponent {
 
+  public counter = signal(10);
+  public squareCounter = computed( () => this.counter() * this.counter() );
+
+  increaseBy( value: number ){
+    this.counter.update( current => current + value);
+  }
 }
