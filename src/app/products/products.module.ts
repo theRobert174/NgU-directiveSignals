bccf8da336a2c1d9e16ebf8a41553eede2fbe510
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-07-08 19:18:36 -0600

    customLabel - custom Directive

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
-import { ReactiveFormsModule } from '@angular/forms';
+
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -15,6 +17,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     ProductsRoutingModule,
     ReactiveFormsModule,
+    SharedModule
   ]
 })
 export class ProductsModule { }
